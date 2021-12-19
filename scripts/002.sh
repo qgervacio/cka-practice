2b331c61e9a089064f0d5c7b685c7c307cb610c5
Quirino Gervacio <qgervacio@gmail.com>
2021-12-20 06:23:49 +0800

    remove misleading comment

--- scripts/002.sh
+++ scripts/002.sh
@@ -4,7 +4,6 @@ set -x # echo all commands
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/high-availability/#steps-for-the-first-control-plane-node
 # https://stackoverflow.com/questions/49553477/kubeadm-join-fails-with-connection-refused-on-one-node-only
 
-# idk sometimes this throws conn timeout. just re-run if that happened
 # note: supply the control plane's address here --apiserver-advertise-address :(
 echo "Create cluster..."
 sudo kubeadm init --pod-network-cidr=192.168.57.0/21 --apiserver-advertise-address=192.168.56.10
