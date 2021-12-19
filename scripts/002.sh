5fe81bd50df99dc44073a6d9ebe0baf3d56eb6f0
Quirino Gervacio <qgervacio@gmail.com>
2021-12-20 06:18:18 +0800

    apply cni

--- scripts/002.sh
+++ scripts/002.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 set -x # echo all commands
 
-echo "Get files for pod network..."
-wget https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-wget https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
-
-echo "Create cluster, specify pod network that matches calico.yaml..."
-cat calico.yaml | grep 192.168.0.0/16
+# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/high-availability/#steps-for-the-first-control-plane-node
+# https://stackoverflow.com/questions/49553477/kubeadm-join-fails-with-connection-refused-on-one-node-only
 
 # idk sometimes this throws conn timeout. just re-run if that happened
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+# note: supply the control plane's address here --apiserver-advertise-address :(
+echo "Create cluster..."
+sudo kubeadm init --pod-network-cidr=192.168.57.0/21 --apiserver-advertise-address=192.168.56.10
 
 echo "Configure account..."
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo mkdir -p /home/vagrant/.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+sudo chown vagrant:vagrant /home/vagrant/.kube/config
 
-echo "Deploy downloaded YAMLs..."
-kubectl apply -f rbac-kdd.yaml
-kubectl apply -f calico.yaml
+echo "Apply CNI plug-in"
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
