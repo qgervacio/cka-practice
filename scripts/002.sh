d15928497dee4705acacd1b1b00cb207db381e14
Quirino Gervacio <qgervacio@gmail.com>
2021-12-20 06:23:06 +0800

    corrected .kube setup

--- scripts/002.sh
+++ scripts/002.sh
@@ -10,9 +10,9 @@ echo "Create cluster..."
 sudo kubeadm init --pod-network-cidr=192.168.57.0/21 --apiserver-advertise-address=192.168.56.10
 
 echo "Configure account..."
-sudo mkdir -p /home/vagrant/.kube
-sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-sudo chown vagrant:vagrant /home/vagrant/.kube/config
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 echo "Apply CNI plug-in"
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
