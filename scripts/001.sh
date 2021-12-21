32da87e61118f12e78b507599e6867319c4dd110
Quirino Gervacio <qgervacio@gmail.com>
2021-12-21 21:31:19 +0800

    added fixed version, aliases and clean-up

--- scripts/001.sh
+++ scripts/001.sh
@@ -23,11 +23,19 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "Add the Kubernetes apt repository..."
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
+# spcify an older version for upgrading purposes
 echo "Update apt package index, install docker kubelet, kubeadm and kubectl, and pin their version..."
 sudo apt-get update
-sudo apt-get install -y docker.io kubelet kubeadm kubectl
+sudo apt-get install -y docker.io kubelet=1.22.5-00 kubeadm=1.22.5-00 kubectl=1.22.5-00
 sudo apt-mark hold docker.io kubelet kubeadm kubectl
 
 echo "Start service..."
 sudo systemctl enable kubelet.service
 sudo systemctl enable docker.service
+
+echo "Clean-up..."
+sudo apt-get autoclean -y
+sudo apt-get autoremove -y
+
+alias k=kubectl
+alias ka=kubeadm
